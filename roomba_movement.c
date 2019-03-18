ee5ca60dee779bdb68123639148e1724ea9ae433
Marcin <mpitera483@gmail.com>
2019-03-18 16:45:53 -0500

    testing light_bump

--- roomba_movement.c
+++ roomba_movement.c
@@ -3,16 +3,17 @@
 int main()
 {
     create_connect();
+    get_create_rlightbump();
+    get_create_llightbump();
     set_create_total_angle(0);
     set_create_distance(0);
     while (get_create_total_angle() < 90) {
         create_drive_direct(-200, 200);   
     }
 
-    while (get_create_total_angle(90) && get_create_distance() < 100) {
-        create_drive_direct(200, 200);
-        msleep(3000);
-    }
+    create_drive_direct(200, 200);
+    while (!get_create_rlightbump() && !get_create_llightbump());
+    
     create_stop();
     create_disconnect();
     return 0;
